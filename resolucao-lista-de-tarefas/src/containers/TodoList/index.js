13c40736bb1d728c5a49074646146b4fd412c474
Brunna Rocha <brunna.17rocha03@gmail.com>
2020-03-10 13:26:29 -0300

    Ajustando as configurações do material UI

--- resolucao-lista-de-tarefas/src/containers/TodoList/index.js
+++ resolucao-lista-de-tarefas/src/containers/TodoList/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import { connect } from "react-redux";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListemItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import IconButton from "@material-ui/core/IconButton";
 
  const TodoList = props => {
      return (
          <List>
             {props.todos.map(todo => (
-                <ListItem>
-                    <ListemItemText primary={todo.title}/>
+                <ListItem button>
+                    <ListItemText primary={todo.title}/>
+                    <ListItemSecondaryAction>
+                        <IconButton>
+                            <DeleteIcon/>
+                        </IconButton>
+                    </ListItemSecondaryAction>
                 </ListItem>
             ))}
         </List>
